b84a8242d075ad37cac221f49c9f4375a346bb99
chiahtarek <chiahtarek@gmail.com>
2026-07-03 23:24:11 -0300

    Register View

--- rotta.com/src/main/java/com/example/rotta/controllers/AuthController.java
+++ rotta.com/src/main/java/com/example/rotta/controllers/AuthController.java
@@ -5,11 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.rotta.dto.RegisterRequestDTO;
 import com.example.rotta.dto.RegisterResponseDTO;
@@ -26,16 +29,23 @@ public class AuthController {
     @Autowired
     UserService userService;
 
+    @GetMapping("/register")
+    public ModelAndView registerGet(){
+        ModelAndView mv = new ModelAndView("register"); 
+        return mv;
+    }
+
     @PostMapping("/register")
-    public ResponseEntity<RegisterResponseDTO> register(@RequestBody RegisterRequestDTO dto) {
+    public ModelAndView registerPost(@ModelAttribute RegisterRequestDTO dto) {
+        ModelAndView mv = new ModelAndView("register"); 
         User user = new User();
         user.setLogin(dto.login());
         user.setPassword(passwordEncoder.encode(dto.password()));
         user.setRole(dto.role());
         userService.register(user);
     
-        return ResponseEntity.status(HttpStatus.CREATED).body(new RegisterResponseDTO(user.getLogin(),user.getPassword())); 
-
+        mv.addObject("successMessage", "Registrado com Sucesso ! "); 
+        return mv;
     }
 
 }
